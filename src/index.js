e9230b3679d0ecf4d63d6ca3c5291debe6158074
Eryk <126007740+er0l18@users.noreply.github.com>
2023-03-09 21:51:35 +0100

    final version

--- src/index.js
+++ src/index.js
@@ -1,58 +1,60 @@
-//import ('./css/styles.css'); //added to index.html
 import { fetchCountries } from './fetchCountries.js';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 500;//better than 300
+Notiflix.Notify.init({
+    width: '360px',
+    position: 'left-top',
+    distance: '75px',
+});
+
+const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector("#search-box");
 const countryList = document.querySelector("ul.country-list");
 
-searchBox.addEventListener("keyup", async () => {
-  const val = searchBox.value;
-  const delay = ms => new Promise(res => setTimeout(res,ms));
-  await delay(DEBOUNCE_DELAY);
-  if (searchBox.value != val) {
-    console.log("User is typing:",val);
-  } else {
-      if (searchBox.value.trim() !== "") {
-        try {
-        const countries = await fetchCountries(searchBox.value.trim());
-        renderCountriesList(countries);
-        } catch (error) {
-        console.log(error.message);
-        }
-      } else {
-        console.log("Input error!");
-        countryList.innerHTML = "";
-      }
+searchBox.addEventListener("input", debounce(async () => {
+if (searchBox.value.trim() !== "") {
+  try {
+    const countries = await fetchCountries(searchBox.value.trim());
+    renderCountriesList(countries);
+  } catch (error) {
+    console.log(error.message);
   }
-});
+} else {
+  console.log("Input error!");
+  countryList.innerHTML = "";
+}
+},DEBOUNCE_DELAY));
 
-  function renderCountriesList(elements) {
-    if (elements.status == 404) {
-      countryList.innerHTML = "Oops, there is no country with that name!";
-    } else if (elements.length >= 10) {
-      console.log(`Too many matches found!\n${elements.length} hits!`);
-      countryList.innerHTML = "Too many matches found. Please enter a more specific name.";
-    } else if (elements.length >= 2) {
+function renderCountriesList(elements) {
+  if (elements.status == 404) {
+    countryList.innerHTML = "";
+    Notiflix.Notify.failure("Oops, there is no country with that name!");
+  } else if (elements.length >= 10) {
+    console.log(`Too many matches found!\n${elements.length} hits!`);
+    countryList.innerHTML = "";
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+  } else if (elements.length >= 2) {
     const markup = elements
       .map(
         (element) => `<li class="countries">
-          <p onclick="onClick(this)" class="clickable">${element.flag} <b>${element.name.official}</b> (${element.name.common})</p>
+          <p>${element.flag} <b>${element.name.official}</b> (${element.name.common})</p>
         </li>`
       )
       .join("");
       countryList.innerHTML = markup;
-    } else {
-      const markup = elements
-      .map(
-        (element) => `<li class="details">
-          <p id="title">${element.flag} <b>${element.name.official}</b> (${element.name.common})</p>
-          <p><b>Capital</b>: ${element.capital}</p>
-          <p><b>Population</b>: ${element.population}</p>
-          <p><b>Languages</b>: ${Object.values(element.languages).join(", ")}</p>
-        </li>`
-      )
-      .join("");
-      countryList.innerHTML = markup;
-    }
-  };
+  } else {
+    const markup = elements
+    .map(
+      (element) => `<li class="details">
+        <p id="title">${element.flag} <b>${element.name.official}</b> (${element.name.common})</p>
+        <p><b>Capital</b>: ${element.capital}</p>
+        <p><b>Population</b>: ${element.population}</p>
+        <p><b>Languages</b>: ${Object.values(element.languages).join(", ")}</p>
+      </li>`
+    )
+    .join("");
+    countryList.innerHTML = markup;
+  }
+};
